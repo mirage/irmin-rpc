5ba0e65eb878692d9130385380d1bfdd37084945
Zach Shipko <zachshipko@gmail.com>
2018-08-17 12:51:13 -0700

    Allow File option for secret key

--- src/irmin-rpc-unix/bin/main.ml
+++ src/irmin-rpc-unix/bin/main.ml
@@ -5,13 +5,18 @@ let config path =
   let head = Git.Reference.of_string "refs/heads/master" in
   Irmin_git.config ~head path
 
-let run host port root contents store =
+let run host port root contents store secret_key =
   let c = Irmin_unix.Cli.mk_contents contents in
   let (module Store) = Irmin_unix.Cli.mk_store store c in
   let module Rpc = Irmin_rpc_unix.Make(Store) in
+  let secret_key =
+    match secret_key with
+    | Some key -> `File key
+    | None -> `Ephemeral
+  in
   let p =
     Store.Repo.v (config root) >>= fun repo ->
-    Rpc.Server.create ~secret_key:`Ephemeral (`TCP (host, port)) repo >>= fun server ->
+    Rpc.Server.create ~secret_key (`TCP (host, port)) repo >>= fun server ->
     Printf.printf "Serving on: %s\n%!" (Uri.to_string (Rpc.Server.uri server));
     Rpc.Server.run server
   in Lwt_main.run p
@@ -36,6 +41,10 @@ let root =
   let doc = "Store location" in
   Arg.(value & opt string "/tmp/irmin" & info ["root"] ~docv:"PATH" ~doc)
 
-let main_t = Term.(const run $ host $ port $ root $ contents $ store)
+let secret_key =
+  let doc = "Secret key" in
+  Arg.(value & opt (some string) None & info ["k"; "secret-key"] ~docv:"FILENAME" ~doc)
+
+let main_t = Term.(const run $ host $ port $ root $ contents $ store $ secret_key)
 let () = Term.exit @@ Term.eval (main_t, Term.info "irmin-rpc")
 
