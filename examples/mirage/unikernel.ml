d2a23dfee042642cd14e763559b24c89e03eb966
Zach Shipko <zachshipko@gmail.com>
2018-08-30 18:06:01 -0700

    Fix addr in mirage example

--- examples/mirage/unikernel.ml
+++ examples/mirage/unikernel.ml
@@ -10,9 +10,12 @@ module Main (Clock: Mirage_clock_lwt.PCLOCK) (Time: Mirage_time_lwt.S) (Stack: M
       let clock = _clock
     end) in
     let port = 8888 in
+    let ipv4 = Stack.ipv4 _stack in
+    let addr = Stack.IPV4.get_ip ipv4 |> List.hd in
+    let addr = Ipaddr.V4.to_string addr in
     Store.Repo.v (Irmin_mem.config ()) >>= fun repo ->
     Logs.info (fun f -> f "STARTING SERVER");
-    Server.create ~secret_key:`Ephemeral _stack repo ~port ~addr:"0.0.0.0" >>= fun server ->
+    Server.create ~secret_key:`Ephemeral _stack repo ~port ~addr >>= fun server ->
     Logs.info (fun f -> f "%s" (Uri.to_string @@ Server.uri server));
     Server.run server
 end
