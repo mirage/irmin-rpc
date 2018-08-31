894631aaca7d85259ec53514ad72c55f2b5b3573
Zach Shipko <zachshipko@gmail.com>
2018-08-31 12:15:59 -0700

    Add get_tree, set_tree client functions

--- src/irmin-rpc/irmin_rpc.ml
+++ src/irmin-rpc/irmin_rpc.ml
@@ -7,7 +7,9 @@ type t = [ `Irmin_b2b5cb4fd15c7d5a ] Capability.t
 module type CLIENT = sig
     module Store: Irmin.S
     val get: t -> ?branch:Store.branch -> Store.key -> (Store.contents, [`Msg of string]) result Lwt.t
+    val get_tree: t -> ?branch:Store.branch -> Store.key -> Store.tree Lwt.t
     val set: t -> ?branch:Store.branch -> ?author:string -> ?message:string -> Store.key -> Store.contents -> Store.Commit.hash Lwt.t
+    val set_tree: t -> ?branch:Store.branch -> ?author:string -> ?message:string -> Store.key -> Store.tree -> Store.Commit.hash Lwt.t
     val remove: t -> ?branch:Store.branch -> ?author:string -> ?message:string -> Store.key -> Store.Commit.hash Lwt.t
     val clone: t -> ?branch:Store.branch -> string -> Store.Commit.hash Lwt.t
     val pull: t -> ?branch:Store.branch -> ?author:string -> ?message:string -> string -> Store.Commit.hash Lwt.t
@@ -34,73 +36,68 @@ end) = struct
   module Store = Store
   module Sync = Irmin.Sync(Store)
 
+  let encode_commit commit cm =
+    let module Commit = Api.Builder.Irmin.Commit in
+    let module Info = Api.Builder.Irmin.Info in
+    let info = Commit.info_init commit in
+    Commit.hash_set commit (Fmt.to_to_string Store.Commit.Hash.pp (Store.Commit.hash cm));
+    let i = Store.Commit.info cm in
+    Info.author_set info (Irmin.Info.author i);
+    Info.message_set info (Irmin.Info.message i);
+    Info.date_set info (Irmin.Info.date i)
+
+  (* Get branch by name and convert it to a capnproto Branch object *)
+  let encode_branch ctx ?branch br =
+      let module Branch = Api.Builder.Irmin.Branch in
+      let commit = Branch.head_init br in
+      let get_branch () = match branch with None -> Store.master ctx | Some name -> Store.of_branch ctx name in
+      get_branch ()>>= fun t ->
+      Store.Head.find t >>= function
+      | Some head ->
+        Branch.name_set br "master" |> ignore;
+        encode_commit commit head;
+        Lwt.return_some br
+      | None -> Lwt.return_none
+
+  (* Convert a Store.tree to capnproto Tree object *)
+  let rec encode_tree tr key (tree: Store.tree): unit Lwt.t =
+    let module Tree = Api.Builder.Irmin.Tree in
+    let module Node = Api.Builder.Irmin.Node in
+    let ks = Fmt.to_to_string Store.Key.pp key in
+    ignore @@ Tree.key_set tr ks;
+    Store.Tree.to_concrete tree >>= function
+    | `Contents (contents, _) ->
+        let _ = Tree.contents_set tr (Fmt.to_to_string Store.Contents.pp contents) in
+        Lwt.return_unit
+    | `Tree l ->
+        Lwt_list.map_p (fun (step, tree) ->
+          let node = Node.init_root () in
+          let step_s = Fmt.to_to_string Store.Key.pp_step step in
+          Node.step_set node step_s;
+          let tt = Node.tree_init node in
+          let tree = Store.Tree.of_concrete tree in
+          encode_tree tt (Store.Key.rcons key step) tree >|= fun () -> node
+        ) l
+        >>= fun l ->
+          let _ = Tree.node_set_list tr l in Lwt.return_unit
+
+  let rec decode_tree tree: Store.Tree.concrete =
+    let module Tree = Api.Reader.Irmin.Tree in
+    let module Node = Api.Reader.Irmin.Node in
+    match Tree.get tree with
+    | Node l ->
+        let l = Capnp.Array.to_list l in
+        `Tree (List.map (fun node ->
+          let step = Node.step_get node |> Store.Key.step_of_string |> unwrap in
+          let tree = Node.tree_get node |> decode_tree in
+          step, tree) l)
+    | Contents c ->
+        let c = Store.Contents.of_string c |> unwrap in
+        `Contents (c, Store.Metadata.default)
+    | Undefined _ -> `Tree []
+
   let local ctx =
     let module Ir = Api.Service.Irmin in
-
-    let encode_commit commit cm =
-        let module Commit = Api.Builder.Irmin.Commit in
-        let module Info = Api.Builder.Irmin.Info in
-        let info = Commit.info_init commit in
-        Commit.hash_set commit (Fmt.to_to_string Store.Commit.Hash.pp (Store.Commit.hash cm));
-        let i = Store.Commit.info cm in
-        Info.author_set info (Irmin.Info.author i);
-        Info.message_set info (Irmin.Info.message i);
-        Info.date_set info (Irmin.Info.date i)
-    in
-
-    (* Get branch by name and convert it to a capnproto Branch object *)
-    let encode_branch ?branch br =
-        let module Branch = Api.Builder.Irmin.Branch in
-        let commit = Branch.head_init br in
-        let get_branch () = match branch with None -> Store.master ctx | Some name -> Store.of_branch ctx name in
-        get_branch ()>>= fun t ->
-        Store.Head.find t >>= function
-        | Some head ->
-          Branch.name_set br "master" |> ignore;
-          encode_commit commit head;
-          Lwt.return_some br
-        | None -> Lwt.return_none
-    in
-
-    (* Convert a Store.tree to capnproto Tree object *)
-    let rec encode_tree tr key (tree: Store.tree): unit Lwt.t =
-      let module Tree = Api.Builder.Irmin.Tree in
-      let module Node = Api.Builder.Irmin.Node in
-      let ks = Fmt.to_to_string Store.Key.pp key in
-      ignore @@ Tree.key_set tr ks;
-      Store.Tree.to_concrete tree >>= function
-      | `Contents (contents, _) ->
-          let _ = Tree.contents_set tr (Fmt.to_to_string Store.Contents.pp contents) in
-          Lwt.return_unit
-      | `Tree l ->
-          Lwt_list.map_p (fun (step, tree) ->
-            let node = Node.init_root () in
-            let step_s = Fmt.to_to_string Store.Key.pp_step step in
-            Node.step_set node step_s;
-            let tt = Node.tree_init node in
-            let tree = Store.Tree.of_concrete tree in
-            encode_tree tt (Store.Key.rcons key step) tree >|= fun () -> node
-          ) l
-          >>= fun l ->
-            let _ = Tree.node_set_list tr l in Lwt.return_unit
-    in
-
-    let rec decode_tree tree: Store.Tree.concrete =
-      let module Tree = Api.Reader.Irmin.Tree in
-      let module Node = Api.Reader.Irmin.Node in
-      match Tree.get tree with
-      | Node l ->
-          let l = Capnp.Array.to_list l in
-          `Tree (List.map (fun node ->
-            let step = Node.step_get node |> Store.Key.step_of_string |> unwrap in
-            let tree = Node.tree_get node |> decode_tree in
-            step, tree) l)
-      | Contents c ->
-          let c = Store.Contents.of_string c |> unwrap in
-          `Contents (c, Store.Metadata.default)
-      | Undefined _ -> `Tree []
-    in
-
     Ir.local @@ object
       inherit Ir.service
 
@@ -169,7 +166,7 @@ end) = struct
         Service.return_lwt (fun () ->
           let resp, results = Service.Response.create Results.init_pointer in
           let br = Results.result_init results in
-          encode_branch br >>= fun _ -> Lwt.return_ok resp)
+          encode_branch ctx br >>= fun _ -> Lwt.return_ok resp)
 
       method get_branch_impl req release_params =
         let open Ir.GetBranch in
@@ -181,7 +178,7 @@ end) = struct
         Service.return_lwt (fun () ->
           let resp, results = Service.Response.create Results.init_pointer in
           let br = Results.result_init results in
-          encode_branch ~branch:name br >>= fun _ -> Lwt.return_ok resp)
+          encode_branch ctx ~branch:name br >>= fun _ -> Lwt.return_ok resp)
 
       method get_tree_impl req release_params =
         let open Ir.GetTree in
@@ -303,6 +300,19 @@ end) = struct
         Capability.call_for_value_exn t method_id req >|= fun res ->
         Store.Contents.of_string (Results.result_get res)
 
+      let get_tree t ?branch key =
+        let open Ir.GetTree in
+        let req, p = Capability.Request.create Params.init_pointer in
+        (match branch with
+        | Some br ->
+            let br = Fmt.to_to_string Store.Branch.pp br in
+            Params.branch_set p br
+        | None -> ());
+        let key_s = Fmt.to_to_string Store.Key.pp key in
+        Params.key_set p key_s |> ignore;
+        Capability.call_for_value_exn t method_id req >|= fun res ->
+        Results.result_get res |> decode_tree |> Store.Tree.of_concrete
+
       let set t ?branch ?author ?message key value =
         let open Ir.Set in
         let req, p = Capability.Request.create Params.init_pointer in
@@ -322,6 +332,30 @@ end) = struct
         let key_s = Fmt.to_to_string Store.Key.pp key in
         Params.key_set p key_s |> ignore;
         Params.value_set p (Fmt.to_to_string Store.Contents.pp value);
+        Capability.call_for_value_exn t method_id req >|= fun res ->
+        let commit = Results.result_get res in
+        Api.Reader.Irmin.Commit.hash_get commit |> Store.Commit.Hash.of_string |> unwrap
+
+      let set_tree t ?branch ?author ?message key tree =
+        let open Ir.SetTree in
+        let req, p = Capability.Request.create Params.init_pointer in
+        (match branch with
+        | Some br ->
+            let br = Fmt.to_to_string Store.Branch.pp br in
+            Params.branch_set p br
+        | None -> ());
+        (match author with
+        | Some author ->
+            Params.author_set p author
+        | _ -> ());
+        (match message with
+        | Some message ->
+            Params.message_set p message
+        | _ -> ());
+        let key_s = Fmt.to_to_string Store.Key.pp key in
+        Params.key_set p key_s |> ignore;
+        let tr = Params.tree_init p in
+        encode_tree tr key tree >>= fun () ->
         Capability.call_for_value_exn t method_id req >|= fun res ->
         let commit = Results.result_get res in
         Api.Reader.Irmin.Commit.hash_get commit |> Store.Commit.Hash.of_string |> unwrap
--- src/irmin-rpc/irmin_rpc.mli
+++ src/irmin-rpc/irmin_rpc.mli
@@ -3,7 +3,9 @@ type t = [ `Irmin_b2b5cb4fd15c7d5a ] Capnp_rpc_lwt.Capability.t
 module type CLIENT = sig
     module Store: Irmin.S
     val get: t -> ?branch:Store.branch -> Store.key -> (Store.contents, [`Msg of string]) result Lwt.t
+    val get_tree: t -> ?branch:Store.branch -> Store.key -> Store.tree Lwt.t
     val set: t -> ?branch:Store.branch -> ?author:string -> ?message:string -> Store.key -> Store.contents -> Store.Commit.hash Lwt.t
+    val set_tree: t -> ?branch:Store.branch -> ?author:string -> ?message:string -> Store.key -> Store.tree -> Store.Commit.hash Lwt.t
     val remove: t -> ?branch:Store.branch -> ?author:string -> ?message:string -> Store.key -> Store.Commit.hash Lwt.t
     val clone: t -> ?branch:Store.branch -> string -> Store.Commit.hash Lwt.t
     val pull: t -> ?branch:Store.branch -> ?author:string -> ?message:string -> string -> Store.Commit.hash Lwt.t
