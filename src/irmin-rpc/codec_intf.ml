5635be249a0866ecd0a04103c0f6ad0b00179938
Craig Ferguson <me@craigfe.io>
2020-04-22 09:53:23 +0200

    Extract usages of Type.{to,of}_string to Codec
    Mostly just code cleanup.
    
    From this point, it's fairly similar to provide a `Make` functor
    parameterised over the codecs that it uses, in case we ever want
    non-generic forms for performance reasons.

--- src/irmin-rpc/codec_intf.ml
+++ src/irmin-rpc/codec_intf.ml
@@ -6,7 +6,23 @@ type info_struct = Builder.Irmin.Info.struct_t
 
 type commit_struct = Builder.Irmin.Commit.struct_t
 
+module type SERIALISABLE = sig
+  type t
+
+  val encode : t -> string
+
+  val decode : string -> (t, [> `Msg of string ]) result
+end
+
 module type MAKER = functor (Store : Irmin.S) -> sig
+  module Branch : SERIALISABLE with type t = Store.branch
+
+  module Key : SERIALISABLE with type t = Store.key
+
+  module Hash : SERIALISABLE with type t = Store.hash
+
+  module Contents : SERIALISABLE with type t = Store.contents
+
   val encode_tree :
     tree_struct builder_t -> Store.key -> Store.tree -> unit Lwt.t
 
