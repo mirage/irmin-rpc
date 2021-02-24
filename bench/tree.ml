7a65e0d68661d83ded9a379f16d3a0334839e767
Zach Shipko <zachshipko@gmail.com>
2021-02-23 18:40:04 -0800

    WIP

--- bench/tree.ml
+++ bench/tree.ml
@@ -343,13 +343,13 @@ let main ncommits ncommits_trace operations_file quick depth width nchain_trees
   Printexc.record_backtrace true;
   Random.self_init ();
   FSHelper.rm_dir config.root;
-  (*let suite =
+  let suite =
       (* The suite contains two `Read_trace benchmarks. To prevent running both of
          them when `Quick is not set, we remove the first one (which is the head
          of the suite as well). *)
       if config.quick then List.filter (fun b -> b.speed = `Quick) suite
       else List.tl suite
-    in*)
+    in
   let run_benchmarks () =
     Lwt_list.fold_left_s
       (fun (config, results) (b : suite_elt) ->
